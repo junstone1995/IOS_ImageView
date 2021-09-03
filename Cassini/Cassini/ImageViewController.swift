cff0112da8d1715679ba4830b328ac5ed53d6f94
Junseok Lee <boris0730@gmail.com>
2021-09-04 00:06:25 +0900

    멀티쓰레딩

--- Cassini/Cassini/ImageViewController.swift
+++ Cassini/Cassini/ImageViewController.swift
@@ -12,17 +12,29 @@ class ImageViewController: UIViewController, UIScrollViewDelegate{
     var imageURL: NSURL?{
         didSet{
             image = nil
-            fetchImage()
+            if view.window != nil{
+                fetchImage()
+            }
         }
     }
     
     private func fetchImage(){
         if let url = imageURL{
-            if let imageData = NSData(contentsOf: url as URL){
-                image = UIImage(data: imageData as Data)
+            DispatchQueue.global(qos: .userInitiated).async {
+                let contentsOfURL = NSData(contentsOf: url as URL)
+                DispatchQueue.main.async {
+                    if url == self.imageURL{
+                        if let imageData = contentsOfURL{
+                            self.image = UIImage(data: imageData as Data)
+                        }
+                    }else{
+                        print("ignored data returned form url \(url)")
+                    }
+                }
             }
         }
     }
+    
     @IBOutlet weak var scrollView: UIScrollView!
     {
         didSet{
@@ -49,6 +61,13 @@ class ImageViewController: UIViewController, UIScrollViewDelegate{
             scrollView?.contentSize = imageView.frame.size
         }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if image == nil{
+            fetchImage()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.addSubview(imageView)
